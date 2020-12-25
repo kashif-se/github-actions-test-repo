0791f218ff58b0bf38b2bfaed434ee8b068cb6a0
kshfse <kashif.ezone@gmail.com>
2020-12-25 05:56:20 +0000

    rabbitmq config read from environment variabels, added docker files for web and bot separately

--- app/utils/notification.js
+++ app/utils/notification.js
@@ -2,8 +2,8 @@ const amqp = require("amqplib/callback_api");
 
 // Producer
 module.exports.notification = (chatId, userId, message) => {
-  const opt = { credentials: require('amqplib').credentials.plain('rabbitmq', 'rabbitmq') };
-  amqp.connect("amqp://localhost", opt, function (error0, connection) {
+  const opt = { credentials: require('amqplib').credentials.plain(process.env.RABBIT_MQ_USER, process.env.RABBIT_MQ_PASS) };
+  amqp.connect("amqp://"+process.env.RABBIT_MQ_SERVER, opt, function (error0, connection) {
     if (error0) {
       throw error0;
     }
